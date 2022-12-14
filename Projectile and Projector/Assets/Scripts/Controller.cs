716a7bb1fdac8eb23cdcaf77de78627775a7308f
StuXet <36419721+StuXet@users.noreply.github.com>
2022-12-14 18:20:01 +0200

    .

--- Projectile and Projector/Assets/Scripts/Controller.cs
+++ Projectile and Projector/Assets/Scripts/Controller.cs
@@ -4,18 +4,59 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    public float sensitivity = 100.0f;
+    //mouse look controller
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    float xRotation = 0f;
+
+    //movement controller
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    Vector3 velocity;
+
+    bool isGrounded;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     void Update()
     {
-        // Get the horizontal and vertical axis values from the mouse
-        float horizontal = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-        float vertical = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        //mouse look controller
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        // Calculate the direction in which the player should be facing
-        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        // Rotate the player to face the direction calculated above
-        transform.rotation = Quaternion.LookRotation(direction);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        //movement controller
+        isGrounded = controller.isGrounded;
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
