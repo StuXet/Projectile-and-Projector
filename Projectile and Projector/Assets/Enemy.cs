ecb716a0a02c5658a2d473dbcd5e4f84494d6e84
StuXet <36419721+StuXet@users.noreply.github.com>
2022-12-21 17:47:14 +0200

    Alon Assets

---
+++ Projectile and Projector/Assets/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, IHittable
+{
+    [SerializeField, Tooltip("The Scriptable Object which holds the correct EnemyStats for this Enemy. See \"Scriptables\" -> \"EnemySOs\" folder.")]
+    EnemyStatsSO enemyStatsSO;
+
+    [SerializeField]
+     EnemyStats stats;
+
+    private void Awake()
+    {
+        Init();
+    }
+    public void Init()
+    {
+        if(enemyStatsSO == null)
+        {
+            Debug.LogError("No EnemySOs set in inspector!");
+            return;
+        }    
+
+        stats = enemyStatsSO.enemyStats;
+    }
+
+
+    public void GetHit(IProjectile projectile)
+    {
+        if (!stats.TakeDamage(projectile.GetDamageValue())) //applies damage, and returns bool isAlive
+        {
+            //enemy has died!
+            Debug.Log($"{name} has died.");
+            Destroy(gameObject, 2); // The enemy will disappear in 2 seconds...
+        }
+    }
+}
---
+++ Projectile and Projector/Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct EnemyStats
+{
+    [ReadOnly]
+    [SerializeField]
+    float currentHP;
+    [ReadOnly]
+    [SerializeField]
+    float maxHP;
+
+    bool isAlive;
+
+    public EnemyStats(float hp)
+    {
+        maxHP = currentHP = hp;
+        isAlive = true;
+    }
+
+
+    ///// <summary>
+    ///// also sets currentHP to newMaxHP 
+    ///// </summary>
+    ///// <param name="newMaxHP"></param>
+    //public void SetMaxHP(float newMaxHP)
+    //{
+    //    maxHP = currentHP = newMaxHP;
+    //}
+    /// <summary>
+    /// returns bool isAlive
+    /// </summary>
+    /// <param name="damage"></param>
+    /// <returns></returns>
+    public bool TakeDamage(float damage)
+    {
+        currentHP -= damage;
+        if(currentHP<=0f)
+        {
+            currentHP = 0; // We don't want to accidentally display a negative value for HP... Unless it is mechanically meaningful.
+                           // There is no technical problem with having negative HP values in your game.
+                           // E.g. If characters in your game may "heal allies to revive them",
+                           // allowing for negative HP value would make it harder to ressurect a character (especially if it was low-on-HP and then took massive damage)
+                           // or if your game doesn't normally display damage to the player, but DOES display currentHP ->
+                           // allowing for a negative HP value will provide valueable information to the Player (mainly, amount of damage delt)
+                           // 
+                           // So there are plenty of good reasons to have/allow-for negative HP values in your game -> but there must be a reason! 
+                           // Because if I look at an HP bar in a game, and I see a negative value (even if just for a second, during their "death animation")
+                           // information reaches my brain.
+                           // "In this game, HP can be negative." - Fact.
+                           // If there is no follow-up on this concept, no game-feature/mechanic/special-ability that uses negative HP,
+                           // and there is no thematic/narrative reason for it being there
+                           // "then why was it there?
+                           // as a mistake? does it have a deeper meaning?
+                           // is this game actually a piece of shit?"
+                           // - and we don't want that, do we?
+            
+            isAlive = false;
+
+            Debug.Log($"Dead");
+        }
+        return isAlive;
+    }
+}
